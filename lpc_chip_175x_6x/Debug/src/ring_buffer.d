8b9fa6a7ef00bde51a28593be686bec47c4db461
Alan Vaudagna <alanvaudagna@gmail.com>
2015-06-09 19:02:29 -0300

    Se agregaron las librerias de chip y board necesarias para usar LPCOPEN en el LPC1769

---
+++ lpc_chip_175x_6x/Debug/src/ring_buffer.d
@@ -0,0 +1,7 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ /home/alan/Desktop/workspace_i2c/lpc_chip_175x_6x/inc/ring_buffer.h \
+ /home/alan/Desktop/workspace_i2c/lpc_chip_175x_6x/inc/lpc_types.h
+
+/home/alan/Desktop/workspace_i2c/lpc_chip_175x_6x/inc/ring_buffer.h:
+
+/home/alan/Desktop/workspace_i2c/lpc_chip_175x_6x/inc/lpc_types.h:
