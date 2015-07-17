92e5c8cb92f233ac3094f4298a00039f8f4a4906
Alan Vaudagna <alanvaudagna@gmail.com>
2015-07-17 13:59:14 -0300

    * Configuracion de interrupcion de Timer cada 1 ms * uint32_t millis : Funcion que devuelve cantidad de ms que pasaron desde la ejecucion del programa * void calibrate_sensors : Inicializa los angulos base que se utilizaran para calibracion, filtra el ruido de lectura. * void set_last_read_angle_data : Guarda los ultimos valores leidos del mpu justo con el momento en ms en que sucedio desde la ejecuciuon del 					  programa
    La demas logica implementada en el main esta a prueba de momento, lo que se hace es:
    
    -Iniciar el timer cada 1 ms
    
    -Guardar la posicion inicial del mpu
    
    -Filtrar el ruido promedio de los sensores a partir de 10 lecturas (raw calibration)
    
    -Leer los valores de los registros del MPU
    
    -En tnow se almacena el momento en ms en el que se efectuo la lectura
    
    -Almacenar los valores obtenidos habiendo desplazado los registros de 8 hacia otros de 16
    
    -Se guarda la temperatura leida
    
    -En gyro_x/y/z se convierte la data leida del giroscopo a grados/s
    
    -En accel_angle_x/y/z se convierte la data leida por el acelerometro en grados, esta es poco sensible ante
     variaciones leves pero es muy sensible ante variaciones bruscas.
    
    -En gyro_angle_x/y/z se procesan los angulos medidos por el giroscopo teniendo en cuenta el dt transcurrido
    
    -En unfiltered_gyro_angle_x/y/z se almacena el valor actual de angulo de orientacion del mpu. El almacenarlo
     de dicho modo sin mas, genera un error de drifting, provocado por la propagacion del error del dt sumarizado
     en el tiempo, se puede corregir esto con el filtro de kalman o el filtro complementario.
    
    -En angle_x/y/z se guarda el resultado de realizar un filtro complementario con alpha = 0.96
     entre el gyro_angle y el accel_angle para x e y, en z se mantiene el gyro_angle_z porque el
     acelerometro no aporta datos para este eje
    
    -Se almacenan los ultimos valores de angulo luego del filtro complementario obtenidos y el tiempo actual de
     lectura ultimo, que se guarda para hacer el siguiente calculo de dt (dt = tnow - get_last_time()).
     Los angulos anteriores se utilizan en el calculo del angulo actual del giroscopo
    
    -Hay una linea de debbug que muestra los angulos del acelerometro obtenidos por consola
    
    -De momento la parte de la UART esa deshabilitada

--- i2c/src/main.c
+++ i2c/src/main.c
@@ -72,6 +72,8 @@
 #define MPU6050_RA_PWR_MGMT_1    0x6B
 #define MPU6050_RA_PWR_MGMT_2    0x6C
 #define MPU6050_PWR1_SLEEP_BIT   6
+
+#define MS_CLOCK_DIVIDER 1000 //usado para obtener la interrupcion cada 1 ms
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -109,6 +111,37 @@ static void initHardware(void)
 
     Chip_I2C_SetClockRate(I2C1, 100000);
 	Chip_I2C_SetMasterEventHandler(I2C1, Chip_I2C_EventHandlerPolling);
+
+	//Configuro la interrupcion de Systic para poder contar la cantidad de milisegundos desde
+	//el inicio del programa (funcion millis en arduino)
+	SysTick_Config(SystemCoreClock / MS_CLOCK_DIVIDER);
+}
+
+
+volatile uint32_t msTicks = 0; // counter for 1ms SysTicks
+
+void SysTick_Handler(void)
+{
+	//Aumento msTicks, que es quien tiene la cantidad de ms que pasaron desde el inicio del programa
+	msTicks++;
+}
+
+//delayTicks: Delay en milisegundos
+void delay (uint32_t delay_ms) {
+  uint32_t current_msTicks;
+
+  current_msTicks = msTicks;	// read current mstick counter
+  // Now loop until required number of ms passes.
+  //msTicks se ira actualizando por la interrupcion de systick
+  while ((msTicks - current_msTicks) < delay_ms);
+}
+
+//Devuelve la cantidad de tiempo en ms que paso desde la ejecucion del programa
+//Necasario para el calculo de gyro angle
+//TODO:ESTO NO PUEDE GENERAR OVERFLOW UNA VEZ LLEGADO AL MAX DE uint32 y EXPLOTA TODO???
+uint32_t millis(void)
+{
+	 return msTicks;
 }
 
 /*==================[external functions definition]==========================*/
@@ -116,6 +149,16 @@ static void initHardware(void)
 //TODO: Leer 10 veces el registro de Acelerometro en X, esto nos dara 5 datos
 //	  	compuestos de 2 bytes cada uno (parte high y low)
 
+
+//  Use the following global variables and access functions
+//  to calibrate the acceleration sensor
+float    base_x_accel;
+float    base_y_accel;
+float    base_z_accel;
+
+float    base_x_gyro;
+float    base_y_gyro;
+float    base_z_gyro;
 
 int main(void)
 {
@@ -151,9 +194,9 @@ int main(void)
 	// Metodo de Escritura
 	//Define el registro que se va a leer en la primera posicion de wbuf
 	//wbuf[0] = MPU6050_RA_ACCEL_XOUT_H; //Parte high de la lectura en x del acelerometro
-							//Como la lectura se realiza de forma secuencial, en la posicion 0
-							//del rbuf ira este dato y en la posicion 1 ira la correspondiente a
-							//la posicion siguiente, osea ACCEL_XOUT_L (0x3C)
+	//Como la lectura se realiza de forma secuencial, en la posicion 0
+	//del rbuf ira este dato y en la posicion 1 ira la correspondiente a
+	//la posicion siguiente, osea ACCEL_XOUT_L (0x3C)
 
 
 	MPU6050_wakeup(&xfer);
@@ -162,17 +205,131 @@ int main(void)
 	wbuf[0] = MPU6050_RA_PWR_MGMT_1;
 	I2C_XFER_config(&xfer, rbuf, 2, MPU6050_I2C_SLAVE_ADDRESS, 0, wbuf, 1);
 
+	//Initialize the angles
+	calibrate_sensors();
+	set_last_read_angle_data(millis(), 0, 0, 0, 0, 0, 0);
+
 
 	//Configuracion de la 1era direccion desde la que se leeran los valores de los registros de los sensores
 	wbuf[0]=MPU6050_RA_ACCEL_XOUT_H;
 	wbuf[1]=0;
 
+
+	double ax ;
+	double ay ;
+	double az ;
+	double gx ;
+	double gy ;
+	double gz ;
+	double arx ;
+	double ary ;
+	double arz ;
+	unsigned long t_now;
+	double dT;
+
 	while(1)
 	{
+		//Leo los valores de los registros de los sensores
 		I2C_XFER_config(&xfer, rbuf, 14, MPU6050_I2C_SLAVE_ADDRESS, 0, wbuf, 1);
 
+		// Obtengo el tiempo en el que se efectuo la lectura para realizar el calculo rotacional
+	    t_now = millis();
+
+	    //Parseo a datos de 16 bits
 		Fill_Samples(&samples, &rbuf);
+
+
+	      // The temperature sensor is -40 to +85 degrees Celsius.
+	      // It is a signed integer.
+	      // According to the datasheet:
+	      //   340 per degrees Celsius, -512 at 35 degrees.
+	      // At 0 degrees: -512 - (340 * 35) = -12412
+	      // samples[3] contiene el valor de la temperatura en unidades de medicion
+	      //Aca se lo pasa a grados celsius implementando la logica de arriba
+	      dT = ( (double) samples[3] + 12412.0) / 340.0;
+
+
+		 ax = samples[0];
+		 ay = samples[1];
+		 az = samples[2];
+		 gx = samples[4];
+		 gy = samples[5];
+		 gz = samples[6];
+
+
+
+		  // Convert gyro values to degrees/sec
+		  float FS_SEL = 131;
+
+		  float gyro_x = (gx - base_x_gyro)/FS_SEL;
+		  float gyro_y = (gy - base_y_gyro)/FS_SEL;
+		  float gyro_z = (gz - base_z_gyro)/FS_SEL;
+
+		  // Get raw acceleration values
+		   //float G_CONVERT = 16384;
+
+		  // Get angle values from accelerometer
+		    float RADIANS_TO_DEGREES = 180/3.14159;
+		  //  float accel_vector_length = sqrt(pow(accel_x,2) + pow(accel_y,2) + pow(accel_z,2));
+		    float accel_angle_y = atan(-1*ax/sqrt(ay*ay) + (az*az))*RADIANS_TO_DEGREES;
+		    float accel_angle_x = atan(ay/sqrt((ax*ax) + (az*az)))*RADIANS_TO_DEGREES;
+		    float accel_angle_z = 0; //No puede obtenerse con el accelerometer
+
+
+		    // Compute the (filtered) gyro angles
+		  //dt esta medido en segundos, por eso se multiplica por 1000 al last_read_time obtenido, que esta en ms
+		  //TODO: El dt es el correcto sobre el que se tiene que hacer las cuentas???
+		  float dt =(t_now - get_last_time())/1000.0;
+		  float gyro_angle_x = gyro_x*dt + get_last_x_angle();
+		  float gyro_angle_y = gyro_y*dt + get_last_y_angle();
+		  float gyro_angle_z = gyro_z*dt + get_last_z_angle();
+
+
+		  // Compute the drifting gyro angles, estos son los que tienen error de drift
+		  float unfiltered_gyro_angle_x = gyro_x*dt + get_last_gyro_x_angle();
+		  float unfiltered_gyro_angle_y = gyro_y*dt + get_last_gyro_y_angle();
+		  float unfiltered_gyro_angle_z = gyro_z*dt + get_last_gyro_z_angle();
+
+
+
+
+		  // Apply the complementary filter to figure out the change in angle - choice of alpha is
+		  // estimated now.  Alpha depends on the sampling rate...
+		  float alpha = 0.96;
+		  float angle_x = alpha*gyro_angle_x + (1.0 - alpha)*accel_angle_x;
+		  float angle_y = alpha*gyro_angle_y + (1.0 - alpha)*accel_angle_y;
+		  float angle_z = gyro_angle_z;  //Accelerometer doesn't give z-angle
+
+		  // Update the saved data with the latest values
+		   set_last_read_angle_data(t_now, angle_x, angle_y, angle_z, unfiltered_gyro_angle_x, unfiltered_gyro_angle_y, unfiltered_gyro_angle_z);
+
+		//Implementacion vieja
+
+		/* arx = (180/3.141592) * atan(ax / sqrt((ay*ay) + (az*az)));
+		 ary = (180/3.141592) * atan(ay / sqrt((ax*ax) + (az*az)));
+		 arz = (180/3.141592) * atan(sqrt((ay*ay) + (ax*ax)) / az);
+
+		 arx =  3.141592 * atan(ax / sqrt((ay*ay) + (az*az)));
+		 ary =  3.141592 * atan(ay / sqrt((ax*ax) + (az*az)));
+		 arz =  3.141592 * atan(sqrt((ay*ay) + (ax*ax)) / az);*/
+
+
+		 //PARTE UART
+	     //Proseguimos a enviar por UART los angulos obtenidos
+		 //float angles [] = {angle_x, angle_y, angle_z};
+		 //UART_Enviar_Angulos(angles);
+
+		 //Impresion de los angulos obtenidos a traves de la consola
+		 //printf("(%f,%f,%f)\n", angle_x, angle_y, angle_z);
+		 //printf("(%f,%f,%f)\n", unfiltered_gyro_angle_x, unfiltered_gyro_angle_y, unfiltered_gyro_angle_z);
+
+
+		 //DEBUG
+	     printf("(%f,%f,%f)\n", accel_angle_x, accel_angle_y, accel_angle_z);
+
 	}
+
+	//Desabilitar_UART();
 
 	return 0;
 }
@@ -250,6 +407,85 @@ void Fill_Samples(uint16_t * samples, uint8_t * rbuf)
 	samples[5]=(rbuf[10] << 8) | rbuf[11];
 	samples[6]=(rbuf[12] << 8) | rbuf[13];
 }
+
+
+void calibrate_sensors() {
+  int                   num_readings = 10;
+  float                 x_accel = 0;
+  float                 y_accel = 0;
+  float                 z_accel = 0;
+  float                 x_gyro = 0;
+  float                 y_gyro = 0;
+  float                 z_gyro = 0;
+  //Serial.println("Starting Calibration");
+
+  // Discard the first 10 set of values read from the IMU
+  //TODO:por que 10? regla empirica?
+  uint8_t wbuf[2] = {MPU6050_RA_ACCEL_XOUT_H};
+  uint8_t rbuf[10] = {0,0,0,0,0,0,0,0,0,0};
+  I2C_XFER_T xfer;
+  uint16_t samples[7] = {0,0,0,0,0,0,0};
+  int i =0;
+  for ( i=0; i< num_readings; i++)
+  {
+	  // Read and average the raw values from the IMU
+
+	  I2C_XFER_config(&xfer, rbuf, 14, MPU6050_I2C_SLAVE_ADDRESS, 0, wbuf, 1);
+	  Fill_Samples(&samples, &rbuf);
+	  x_accel += samples[0];
+	  y_accel += samples[1];
+	  z_accel += samples[2];
+	  x_gyro += samples[4];
+	  y_gyro += samples[5];
+	  z_gyro += samples[6];
+	  delay(100);
+  }
+  x_accel /= num_readings;
+  y_accel /= num_readings;
+  z_accel /= num_readings;
+  x_gyro /= num_readings;
+  y_gyro /= num_readings;
+  z_gyro /= num_readings;
+
+  // Store the raw calibration values globally
+  base_x_accel = x_accel;
+  base_y_accel = y_accel;
+  base_z_accel = z_accel;
+  base_x_gyro = x_gyro;
+  base_y_gyro = y_gyro;
+  base_z_gyro = z_gyro;
+}
+
+// Use the following global variables and access functions to help store the overall
+// rotation angle of the sensor
+unsigned long last_read_time;
+float         last_x_angle;  // These are the filtered angles
+float         last_y_angle;
+float         last_z_angle;
+float         last_gyro_x_angle;  // Store the gyro angles to compare drift
+float         last_gyro_y_angle;
+float         last_gyro_z_angle;
+
+//Devuelve el momento en el que se hizo la ultima lectura, en milisegundos desde la ejecucion del programa
+unsigned long get_last_time() {return last_read_time;}
+float get_last_x_angle() {return last_x_angle;}
+float get_last_y_angle() {return last_y_angle;}
+float get_last_z_angle() {return last_z_angle;}
+float get_last_gyro_x_angle() {return last_gyro_x_angle;}
+float get_last_gyro_y_angle() {return last_gyro_y_angle;}
+float get_last_gyro_z_angle() {return last_gyro_z_angle;}
+
+
+void set_last_read_angle_data(unsigned long time, float x, float y, float z, float x_gyro, float y_gyro, float z_gyro) {
+  last_read_time = time;
+  last_x_angle = x;
+  last_y_angle = y;
+  last_z_angle = z;
+  last_gyro_x_angle = x_gyro;
+  last_gyro_y_angle = y_gyro;
+  last_gyro_z_angle = z_gyro;
+}
+
 
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
